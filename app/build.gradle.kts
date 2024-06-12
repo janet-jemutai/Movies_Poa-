6b131e0f9fd42e7a510d75d676c6f0c15eedad66
Jemutai <janetjemutai19@gmail.com>
2024-06-12 11:26:50 +0300

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,78 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    //id "kotlin-kapt"
+
+}
+
+android {
+    namespace = "com.jemutai.moviespoaapp"
+    compileSdk = 34
+
+
+    buildFeatures{
+        dataBinding = true
+    }
+    defaultConfig {
+        applicationId = "com.jemutai.moviespoaapp"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+
+//    implementation (libs.kotlin.stdlib)
+//    implementation (libs.androidx.core.ktx.v160)
+//    implementation (libs.androidx.appcompat.v131)
+//    implementation (libs.material.v140)
+//    implementation (libs.androidx.constraintlayout.v210)
+//    implementation (libs.androidx.lifecycle.extensions)
+//    implementation (libs.androidx.lifecycle.viewmodel.ktx)
+//    implementation (libs.androidx.lifecycle.livedata.ktx)
+//    implementation (libs.androidx.navigation.fragment.ktx)
+//    implementation (libs.androidx.navigation.ui.ktx)
+//    implementation (libs.kotlinx.coroutines.core)
+//    implementation (libs.kotlinx.coroutines.android)
+//    implementation (libs.koin.android)
+//    implementation (libs.koin.androidx.viewmodel)
+//    implementation (libs.retrofit)
+//    implementation (libs.converter.gson)
+//    implementation (libs.logging.interceptor)
+//
+//
+//    implementation (libs.material.v140)
+//    implementation (libs.androidx.viewpager2)
+//    implementation (libs.androidx.fragment.ktx)
+}
